9af714913d8abb1ea559481cfa966014ccb2b881
bruhhwhat <120732934+bruhhwhat@users.noreply.github.com>
2024-11-08 08:20:12 +0530

    inital commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+# Dockerfile
+FROM openjdk:11-jdk-slim
+WORKDIR /app
+COPY /target/*.jar /app
+EXPOSE 8081
+ENTRYPOINT ["java","-jar","app.jar"]
---
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+    public void testMessage() {
+        Application app = new Application();
+        assertEquals("Hello, CI/CD Pipeline!", app.getMessage());
+    }
+}
